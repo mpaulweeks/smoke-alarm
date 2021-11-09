53edaecf4a890369af3ba278a3ffb3a45510d151
mpaulweeks <mpaulweeks@gmail.com>
2021-11-09 15:37:15 -0500

    add more endpoints

--- src/config.ts
+++ src/config.ts
@@ -28,12 +28,63 @@ export const config: SmokeAlarmConfig = {
     label: 'toughlovearena.com',
     endpoints: [{
       url: 'https://toughlovearena.com/version.json',
+      verify: resp => ({
+        ok: !!resp.json,
+        message: resp.json?.v,
+      }),
+    }],
+  }, {
+    label: 'cache.tla',
+    endpoints: [{
+      url: 'https://cache.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'handshake.tla',
+    endpoints: [{
+      url: 'https://handshake.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'leaderboard.tla',
+    endpoints: [{
+      url: 'https://leaderboard.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'match.tla',
+    endpoints: [{
+      url: 'https://match.toughlovearena.com',
       verify: verifyJson,
     }],
   }, {
     label: 'presence.tla',
     endpoints: [{
       url: 'https://presence.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'lobbya.tla',
+    endpoints: [{
+      url: 'https://lobbya.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'lobbyb.tla',
+    endpoints: [{
+      url: 'https://lobbyb.toughlovearena.com/health',
+      verify: verifyJson,
+    }],
+  }, {
+    label: 'stun.tla',
+    endpoints: [{
+      url: 'https://stun.toughlovearena.com/health',
+    }],
+  }, {
+    label: 'serverless accounts API	',
+    endpoints: [{
+      url: 'https://us-central1-fighter-api.cloudfunctions.net/webApi/api/v1/',
+      verify: verifyJson,
     }],
   }],
 };
