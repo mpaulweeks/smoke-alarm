e0d89234b7b8ddba967f77ed6206f6c450e36424
mpaulweeks <mpaulweeks@gmail.com>
2021-11-09 12:06:32 -0500

    add TLA auth and endpoints

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,9 @@
+import fs from 'fs';
 import { SmokeAlarmConfig } from "../lib/types";
 
 const oneMinute = 1000 * 60;
+
+const auth: { awsKey: string, awsSecret: string } = JSON.parse(fs.readFileSync('auth.json').toString());
 
 export const config: SmokeAlarmConfig = {
   // default values
@@ -8,7 +11,25 @@ export const config: SmokeAlarmConfig = {
   positiveIntervalMS: oneMinute * 60,
 
   // todo fill in with your service
-  recipients: [],
-  auth: {},
-  services: [],
+  recipients: [
+    'toughlovearena@gmail.com',
+  ],
+  auth: {
+    awsSes: {
+      from: 'toughlovearena.login@gmail.com',
+      key: auth.awsKey,
+      secret: auth.awsSecret,
+    },
+  },
+  services: [{
+    label: 'toughlovearena.com',
+    endpoints: [{
+      url: 'https://toughlovearena.com/version.json',
+    }],
+  }, {
+    label: 'presence.tla',
+    endpoints: [{
+      url: 'https://presence.toughlovearena.com/health',
+    }],
+  }],
 };
